4e1b6456959cac4a63fa185a13375591593ae09f
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-11-08 09:47:27 +0800

    Added my actual project files for teacher

---
+++ project1/src/App.jsx
@@ -0,0 +1,14 @@
+import { rootGreet } from './data/RootModule';
+
+function App() {
+  rootGreet();
+
+  return (
+    <div>
+      <h1>React App Running</h1>
+      <p>Check your browser console</p>
+    </div>
+  );
+}
+
+export default App;
